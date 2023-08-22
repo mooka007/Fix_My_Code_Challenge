0267854c6ea457d7d0579a50c521d72dd2e2b373
mooka007 <mooka3084@gmail.com>
2023-08-21 19:13:47 -0700

    fix it

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
-""" FizzBuzz
-"""
+""" FizzBuzz """
 import sys
 
 
 def fizzbuzz(n):
     """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
+    The FizzBuzz function outputs a sequence of numbers from 1 to n,
+    with each number separated by a space. However, there are certain conditions:
 
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
+    If a number is a multiple of three, it is replaced with the word "Fizz".
+    If a number is a multiple of five, it is replaced with the word "Buzz".
+    If a number is a multiple of both three and five, it is replaced with the phrase "FizzBuzz".
     """
     if n < 1:
         return
 
-    tmp_result = []
+    result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
+        if (i % 3) == 0 and (i % 5) == 0:
+            result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            result.append("Fizz")
         elif (i % 5) == 0:
-            tmp_result.append("Buzz")
+            result.append("Buzz")
         else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
+            result.append(str(i))
+    print(" ".join(result))
 
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Missing The first argument 'Number' ")
+        print("How to run it: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
-
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+    else:
+        number = int(sys.argv[1])
+        fizzbuzz(number)
